bc42d6ac0a8bd80f9bcba53285e00770e452e477
Glitch (hello-webpage) <none>
2020-12-21 22:42:12 +0000

    👴👞 Checkpoint ./main.js:2372395/941 ./index.html:2372395/49

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 let startMenu = {
-  
+  dotInterval: function() {},
+  init: function() {
+    let text = document.getElementById("mainText");
+    let dots = 0;
+    this.dotInterval = setInterval(() => {
+      let dotText = "";
+      for (let i = 0; i < dots; i ++) dotText += ".";
+      text.textContent = "Loading" + dotText;
+      dots ++;
+      if (dots + 1 > 4) dots = 0;
+    }, 500);
+  },
+  loaded: function() {
+    clearInterval(this.dotInterval);
+    setTimeout(() => document.getElementById("mainText").textContent = "Diep", 500);
+  }
 };
+startMenu.init();
 window.onload = function() {
   console.log("Loaded assets.");
-  startMenu.init();
+  startMenu.loaded();
 };
