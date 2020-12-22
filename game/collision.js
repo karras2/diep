a27fe0f0286b58b922e9f3e4056927680f3bf69b
Glitch (hello-webpage) <none>
2020-12-22 22:02:17 +0000

    🎣🛤 Checkpoint ./game/collision.js:2372395/330

--- game/collision.js
+++ game/collision.js
@@ -1,20 +1,20 @@
 let basicCollide = (i, o) => {
   if (i.collisionArray.includes(o) || o.collisionArray.includes(i)) return;
-  
+  if (i === o || i.master === o || o.master === i || o.master === i.master) return;
   i.collisionArray.push(o);
   o.collisionArray.push(i);
   let fasterEntity = o;
   if (Math.abs(i.vx + i.vy) + i.size > Math.abs(o.vx + o.vy) + o.size) fasterEntity = i;
   if (fasterEntity === i) {
-    i.vx *= 0.75;
-    i.vy *= 0.75;
-    o.vx = i.vx * 0.1;
-    o.vy = i.vy * 0.1;
+    o.vx = i.vx;
+    o.vy = i.vy;
+    i.vx *= -0.75;
+    i.vy *= -0.75;
   } else {
-    o.vx *= 0.75;
-    o.vy *= 0.75;
-    i.vx = o.vx * 0.1;
-    i.vy = o.vy * 0.1;
+    i.vx = o.vx;
+    i.vy = o.vy;
+    o.vx *= -0.75;
+    o.vy *= -0.75;
   }
 };
 
